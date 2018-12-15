5a06cec59776e65bfb63d115ca4d0a316084cd22
flywithbug <369495368@qq.com>
2018-12-15 19:25:54 +0800

    pushtest

--- push.sh
+++ push.sh
@@ -5,9 +5,10 @@ info() {
      local normal="\033[0m"
      echo -e "[${green}INFO${normal}] $1"
 }
+echo $#
 
 cur_date="`date +%Y-%m-%d-%H:%M:%S`"
-dif="$1"
+dif=$1
 if [ -z "dif" ]; then
     echo "commit msg is empty。
     use cur_date as commit info"
