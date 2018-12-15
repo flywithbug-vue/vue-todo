5a7e54556b036bb9e07afb85a6cc444fecdd5222
flywithbug <369495368@qq.com>
2018-12-15 20:26:07 +0800

    push.sh修改

--- push.sh
+++ push.sh
@@ -6,10 +6,16 @@ info() {
      echo -e "[${green}INFO${normal}] $1"
 }
 
+error() {
+     local red="\033[1;31m"
+     local normal="\033[0m"
+     echo -e "[${red}ERROR${normal}] $1"
+}
+
 cur_date="`date +%Y-%m-%d-%H:%M:%S`"
 dif=$*
 if [ -z "dif" ]; then
-    echo "commit msg is empty。
+    info "commit msg is empty。
     use cur_date as commit info"
     dif=$cur_date
 fi
@@ -17,3 +23,5 @@ fi
 git add .
 git commit -m $dif
 git push -u origin master
+
+
