398dd3e7cd7ad3f4f5f448e2ef8e02e207cc4ba7
flywithbug <369495368@qq.com>
2018-12-15 19:16:25 +0800

    push.sh

--- push.sh
+++ push.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+
 cur_date="`date +%Y-%m-%d-%H:%M:%S`"
 dif="$1"
 if [ -z "dif" ]; then
+    echo "commit msg is empty。
+    use cur_date as commit info"
     dif=$cur_date
 fi
 
