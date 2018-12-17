1c0fcd11a6bd40aedab56b8c94d99d9828e77cad
flywithbug <369495368@qq.com>
2018-12-17 18:28:47 +0800

    model modify

--- server/handler/login_handler.go
+++ server/handler/login_handler.go
@@ -1,11 +1,12 @@
 package handler
 
 import (
-	log "github.com/flywithbug/log4go"
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"todo-go/core/jwt"
 	"todo-go/model"
+
+	log "github.com/flywithbug/log4go"
+	"github.com/gin-gonic/gin"
 )
 
 func LoginHandler(ctx *gin.Context) {
@@ -19,17 +20,17 @@ func LoginHandler(ctx *gin.Context) {
 		aRes.SetErrorInfo(http.StatusBadRequest, "para invalid"+err.Error())
 		return
 	}
-	u, err := model.FindUserByAccountPass(login.Account, login.Password)
+	u, err := model.CheckLogin(login.Account, login.Password)
 	if err != nil {
 		log.Error(err.Error())
-		aRes.SetErrorInfo(http.StatusInternalServerError, "account or password not right")
+		aRes.SetErrorInfo(http.StatusUnauthorized, "account or password not right")
 		return
 	}
 	claims := jwt.NewCustomClaims(u.UserId, u.Account)
 	token, err := jwt.GenerateToken(claims)
 	if err != nil {
 		log.Error(err.Error())
-		aRes.SetErrorInfo(http.StatusInternalServerError, "token generate error"+err.Error())
+		aRes.SetErrorInfo(http.StatusUnauthorized, "token generate error"+err.Error())
 		return
 	}
 	aRes.SetResponseDataInfo("token", token)
