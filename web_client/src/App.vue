c56aabc9abe92e1ca99a04c9effe1f34272cace2
flywithbug <369495368@qq.com>
2018-12-20 01:10:30 +0800

    App style modify

--- web_client/src/App.vue
+++ web_client/src/App.vue
@@ -27,15 +27,6 @@ export default {
     margin: 0 auto;
   }
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  font-size: 24px;
-}
   .logo {
     display: block;
     margin: 20px auto;
