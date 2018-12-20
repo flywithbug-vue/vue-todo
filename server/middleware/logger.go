a96c5fee0a5f2caf7717ab6d24316b7cb3f019c8
flywithbug <369495368@qq.com>
2018-12-20 10:58:28 +0800

    infoo

--- server/middleware/logger.go
+++ server/middleware/logger.go
@@ -36,7 +36,7 @@ func Logger(notlogged ...string) gin.HandlerFunc {
 		//	)
 		//}
 		headers, _ := json.Marshal(c.Request.Header)
-		log.Info("[GIN] [%s] [Started]\tRequestHeader::%s\n", xReqid, headers)
+		log.Infoo("[GIN] [%s] [Started]\tRequestHeader::%s\n", xReqid, headers)
 		// Process request
 		c.Next()
 		// Log only when path is not being skipped
